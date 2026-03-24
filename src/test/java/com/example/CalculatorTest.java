8a8d8960360b708af4533968e62f9437b9560ced
Aravinthan Narasimhan <aravin.cse@gmail.com>
2026-03-24 18:08:50 +0530

    first commit message for calculator-app

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAddWithPositiveNumbers() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testAddWithNegativeNumbers() {
+        assertEquals(-5, calculator.add(-2, -3));
+    }
+
+    @Test
+    void testAddWithZero() {
+        assertEquals(7, calculator.add(7, 0));
+    }
+
+    @Test
+    void testSubtractWithPositiveNumbers() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    void testSubtractWithNegativeResult() {
+        assertEquals(-1, calculator.subtract(2, 3));
+    }
+
+    @Test
+    void testSubtractWithZero() {
+        assertEquals(7, calculator.subtract(7, 0));
+    }
+}
